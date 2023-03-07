2f86834528c96a012f91b75f08eb6164b5ceb73b
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-03-07 10:36:23 +0530

    Update recursionsProblems.cpp

--- recursionsProblems.cpp
+++ recursionsProblems.cpp
@@ -16,3 +16,22 @@ int main() {
 
     return 0;
 }
+
+// Power of two
+#include <iostream>
+using namespace std;
+
+int power(int n){
+    if(n==0)
+     return 1;
+    return 2*power(n-1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int ans = power(n);
+    cout << ans << endl;
+
+    return 0;
+}
