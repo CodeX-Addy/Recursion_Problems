6a64b92840253b1ae42a907385a5da8dc73a58b0
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-03-07 11:46:59 +0530

    Update recursionsProblems.cpp

--- recursionsProblems.cpp
+++ recursionsProblems.cpp
@@ -35,3 +35,24 @@ int main() {
 
     return 0;
 }
+
+// Print counting
+#include <iostream>
+using namespace std;
+void printCount(int n){
+    if(n==0)
+     return ;
+     
+    cout << n << " ";
+    printCount(n-1);
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    printCount(n);
+    
+
+    return 0;
+}
