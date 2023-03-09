c072b5591747778372dd8cf88005877f460bbebd
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-03-09 10:27:03 +0530

    Update problemType2.cpp

--- problemType2.cpp
+++ problemType2.cpp
@@ -1,3 +1,4 @@
+// Checking if array is sorted or not
 #include <iostream>
 using namespace std;
 
@@ -22,5 +23,33 @@ int main() {
     else{
         cout << "Array is not sorted" << endl;
     }
+    return 0;
+}
+
+// Returning the sum of all elements of the given array
+#include <iostream>
+using namespace std;
+
+int getSum(int *arr, int size){
+    if(size==0)
+     return 0;
+     
+    if(size==1)
+     return arr[0];
+     
+    else{
+        int remainingPart = getSum(arr+1, size-1);
+        int sum = arr[0] + remainingPart;
+        return sum;
+    }
+}
+
+int main() {
+    int arr[] = {1,2,5,8,10};
+    int size = 5;
+    
+    int ans = getSum(arr,size);
+    cout << ans << endl;
+
     return 0;
 }
