63f5ef5dd98835af3fc6c2e7175cbb1800e3eede
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-03-09 11:31:32 +0530

    Update searching.cpp

--- searching.cpp
+++ searching.cpp
@@ -27,3 +27,35 @@ int main() {
 
     return 0;
 }
+
+// BINARY SEARCH
+#include <iostream>
+using namespace std;
+
+bool binarySearch(int *arr, int s, int e, int k){
+    // element is not present
+    if(s>e)
+     return false;
+    int mid = s + (e-s)/2;
+    if(arr[mid] == k)// if element is present at mid
+     return true;
+     
+    if(arr[mid] < k)
+     return binarySearch(arr, mid+1, e, k);
+     
+    return binarySearch(arr, s, mid-1, k);
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5,6};
+    int size = 6;
+    bool ans = binarySearch(arr,0, 5, 2);
+    if(ans){
+        cout << "Key is present" << endl;
+    }
+    else{
+        cout << "Key is not present" << endl;
+    }
+
+    return 0;
+}
