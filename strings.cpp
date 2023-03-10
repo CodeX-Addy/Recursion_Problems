d87fd07cc3961e178fee15d717c5cd14938688b0
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-03-10 20:39:58 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -17,3 +17,20 @@ int main() {
     cout << name << endl;
     return 0;
 }
+// Using single pointer approach
+#include <iostream>
+using namespace std;
+void reverseString(string& str, int i){
+    int n = str.length()-1;
+    if(i>n-i)
+     return;
+    swap(str[i],str[n-i]);
+    i++;
+    reverseString(str, i);
+}
+int main() {
+    string name = "tantheta";
+    reverseString(name, 0);
+    cout << name << endl;
+    return 0;
+}
