cad9e50a6eb0d80d126c20da7e9a08cb722e60e0
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-10-21 21:38:03 +0530

    backtracking

--- permutation.cpp
+++ permutation.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void permutation(string st, int ind){
+void permutation(string& st, int ind){
     if(ind>=st.length()){
         cout << st << " ";
         return;
@@ -9,6 +9,8 @@ void permutation(string st, int ind){
     for(int j=ind; j<st.length(); ++j){
         swap(st[ind], st[j]);
         permutation(st, ind+1);
+        //Backtracking
+        swap(st[ind], st[j]);
     }
 }
 int main() {
