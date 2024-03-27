1623804c4d18caddccd8e9b4963243d51eae14ba
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-03-27 19:29:24 +0530

    last occurence of a character

--- lastOcc.cpp
+++ lastOcc.cpp
@@ -1,61 +1,25 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-//Left to right
-// int lastOcc(string s, char ch, int i, int& ans){
-    
-    
-//     //base case
-//     if(i>=s.length()) return -1;
-    
-    
-//     //processing waala part
-//     if(s[i] == ch){
-//         ans = i;
-//     }
-    
-//     //recursive call
-//     lastOcc(s, ch, i+1, ans);
-// }
-
-
-// int main(){
-//     string st;    cin >> st;
-    
-//     cout << "Enter the character to find: " << endl;
-//     char ch;cin >> ch;
-    
-//     int ans = -1;
-//     lastOcc(st, ch, 0, ans);
-//     cout << ans << endl;
-// }
-
-//Right to left
-void lastOcc(string s, char ch, int i, int& ans){
-    
-    
+int lastOcc(string &str, char ch, int index){
     //base case
-    if(i<=0) return;
-    
-    
-    //processing waala part
-    if(s[i] == ch){
-        ans = i;
-        return;
+    if(index <= 0)
+        return -1;
+    int ans = 0;
+
+    if(str[index] == ch){
+        ans = index;
+        return ans;
     }
-    
-    //recursive call
-    lastOcc(s, ch, i-1, ans);
+
+    lastOcc(str, ch, index-1);
+
 }
 
-
 int main(){
-    string st;    cin >> st;
-    
-    cout << "Enter the character to find: " << endl;
-    char ch;cin >> ch;
-    
-    int ans = -1;
-    lastOcc(st, ch, st.length()-1, ans);
-    cout << ans << endl;
+    string str = "aabbab";
+    char ch = 'b';
+
+    cout << "Occurence of " << ch << " is " << lastOcc(str, ch,str.length()-1) << endl;
 }
